b7b3230e4f135c7be1534f7212cd3622a47d39a9
MohanRamV <vemurimohanram@gmail.com>
2025-03-29 19:02:30 -0500

    Created Project Structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen_AI_Project',
+    version= '0.0.0',
+    author= 'Mohan Ram Vemuri',
+    author_email= 'mohanramv29@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
